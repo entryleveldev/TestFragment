fb2fd08b072e5c39748d3311c02ee2372846114a
entryleveldev <ptanapaisankit@Prat-PC.AetherPal.local>
2013-05-28 11:18:14 -0400

    fixed overlap

--- src/com/entrylevdev/testfrag/MainActivity.java
+++ src/com/entrylevdev/testfrag/MainActivity.java
@@ -1,6 +1,5 @@
 package com.entrylevdev.testfrag;
 
-
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTabHost;
@@ -8,29 +7,57 @@ import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.Menu;
 import android.widget.TabHost;
-import android.widget.TabHost.TabSpec;
 
 public class MainActivity extends FragmentActivity {
-		
+
 	private FragmentTabHost mHost;
-	
+	private String TAG = "MainActivity";
+	private EntertainmentFragment enFrag;
+	private SchoolFragment schoolFrag;
+	private SportsFragment sportFrag;
+
 	public void changeFragment() {
 		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        EntertainmentFragment enFragment = new EntertainmentFragment();
-        ft.replace(R.id.tabcontent, enFragment);
-        ft.commit();
+		enFrag = new EntertainmentFragment();
+		ft.replace(R.id.tabcontent, enFrag);
+		// ft.addToBackStack(null);
+		ft.commit();
 	}
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		mHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
-	    mHost.setup(this, getSupportFragmentManager(), R.id.tabcontent);
-        mHost.addTab(mHost.newTabSpec("School")
-                .setIndicator("School"), SchoolFragment.class, null);
-        mHost.addTab(mHost.newTabSpec("Sport")
-                .setIndicator("Sport"), SportsFragment.class, null);
+		mHost.setup(this, getSupportFragmentManager(), R.id.tabcontent);
+		mHost.addTab(mHost.newTabSpec("School").setIndicator("School"),
+				SchoolFragment.class, null);
+		mHost.addTab(mHost.newTabSpec("Sport").setIndicator("Sport"),
+				SportsFragment.class, null);
+		mHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+
+			@Override
+			public void onTabChanged(String tabId) {
+				// TODO Auto-generated method stub
+				if (tabId.equalsIgnoreCase("School")) {
+					Log.v(TAG, "school");
+					FragmentTransaction ft = getSupportFragmentManager()
+							.beginTransaction();
+					schoolFrag = new SchoolFragment();
+					ft.replace(R.id.tabcontent, schoolFrag);
+					ft.commit();
+				}
+				if (tabId.equalsIgnoreCase("Sport")) {
+					Log.v(TAG, "Sport");
+					FragmentTransaction ft = getSupportFragmentManager()
+							.beginTransaction();
+					SportsFragment sportFrag = new SportsFragment();
+					ft.replace(R.id.tabcontent, sportFrag);
+					ft.commit();
+				}
+			}
+
+		});
 	}
 
 	@Override
